5a71dbc295c418b178077275dd000489fd049c0d
nocarefree <170890803@qq.com>
2022-05-04 10:57:58 +0800

    dev2

---
+++ src/Models/User.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ncf\Lehr;
+
+use \Illuminate\Database\Eloquent\Model;
+
+class User extends Model{
+
+}
---
+++ src/Models/UserGroup.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ncf\Lehr;
+
+use \Illuminate\Database\Eloquent\Model;
+
+class UserGroup extends Model{
+
+}
